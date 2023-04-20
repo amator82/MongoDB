dec4716becc834b325f39f4a120ffb922600f408
amator82 <ilyakorckishko@gmail.com>
2023-04-20 14:34:07 +0300

    get one movie and delete one

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
+const ObjectId = require('mongodb').ObjectId
 
 const PORT = 3000
 
@@ -16,5 +17,48 @@ connectToDb((err) => {
         db = getDb()
     } else {
         console.log(`DB connection error ${err}`)
+    }
+})
+
+const handleError = (res, error) => {
+    res.status(500).json({ error })
+}
+
+app.get('/movies', (req, res) => {
+    const movies = []
+
+    db.collection('movies')
+        .find() // cursor - hasNext, next, forEach
+        .sort({ title: 1 })
+        .forEach((movie) => movies.push(movie))
+        .then(() => {
+            res.status(200).json(movies)
+        })
+        .catch(() => handleError(res, 'Something goes wrong...'))
+})
+
+app.get('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('movies')
+            .findOne({ _id: new ObjectId(req.params.id) })
+            .then((doc) => {
+                res.status(200).json(doc)
+            })
+            .catch(() => handleError(res, 'Something goes wrong...'))
+    } else {
+        res.status(500).json({ error: 'Wrong Id' })
+    }
+})
+
+app.delete('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('movies')
+            .deleteOne({ _id: new ObjectId(req.params.id) })
+            .then((result) => {
+                res.status(200).json(result)
+            })
+            .catch(() => handleError(res, 'Something goes wrong...'))
+    } else {
+        handleError(res, 'Wrong Id')
     }
 })
